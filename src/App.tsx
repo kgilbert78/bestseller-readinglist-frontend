3ed434adf9efe88edaf3298f4a7edbf361973cf0
kgilbert78 <kgilbert78@gmail.com>
2022-08-21 22:44:35 -0400

    started creating state

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { Bestsellers } from "./Bestsellers";
 import { SideNav } from "./SideNav";
 import { TopNav } from "./TopNav";
 import sampleReadingList from "./sampleReadingList.json";
 
+// NYT stuff I care about
+export interface Bestseller {
+  title: string;
+  author: string;
+  description: string;
+  coverImg: string;
+  amazonLink: string;
+  rank: number; // rank *within* category
+  // make a "Category" interface to reflect incoming data structure or is this enough?
+  category: string;
+  categoryDisplay: string;
+}
+
+// for/from user's reading list in my database
+interface ReadingListBook {
+  title: string;
+  author: string;
+  amazonLink: string;
+  bookID: number;
+  createdAt: string; // timestamp? comes in as string though.
+  updatedAt: string; // timestamp? comes in as string though.
+  didRead: boolean;
+  category: string;
+  categoryDisplay: string;
+}
+
+interface User {
+  username: string;
+  accessCode: string;
+}
+
 function App() {
-  // NYT stuff I care about
-  interface Bestseller {
-    title: string;
-    author: string;
-    description: string;
-    coverImg: string;
-    amazonLink: string;
-    rank: number; // rank *within* category
-    // make a "Category" interface to reflect incoming data structure or is this enough?
-    category: string;
-    categoryDisplay: string;
-  }
+  const [bestsellerList, setBestsellerList] =
+    useState<Array<Bestseller> | null>(null);
 
-  // for/from user's reading list in my database
-  interface ReadingListBook {
-    title: string;
-    author: string;
-    amazonLink: string;
-    bookID: number;
-    createdAt: string; // timestamp? comes in as string though.
-    updatedAt: string; // timestamp? comes in as string though.
-    didRead: boolean;
-    category: string;
-    categoryDisplay: string;
-  }
-
-  interface User {
-    username: string;
-    accessCode: string;
-  }
-
-  const loadBestsellers = async () => {
+  const loadBestsellers = useCallback(async () => {
     const response = await fetch(
       "https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=jhQErSJStIHawxkBeOcyPHcP0nC3O5Dw",
       {
@@ -47,8 +50,15 @@ function App() {
     );
     const newYorkTimesData = await response.json();
     // console.log(newYorkTimesData);
-    console.log(newYorkTimesData.results.lists);
-  };
+    // console.log(newYorkTimesData.results.lists);
+    if (!bestsellerList) {
+      // setBestsellerList(newYorkTimesData.results.lists);
+      for (let eachPart of newYorkTimesData) {
+        // make an object that matches the format of the Bestseller interface and append to a list variable, to pass to setBestsellerList after loop finishes
+      }
+    }
+    console.log("bestsellerList in App:", bestsellerList);
+  }, [bestsellerList]); // bestsellerList
 
   const getReadingListFromDB = async () => {
     let dbData = sampleReadingList;
@@ -72,14 +82,14 @@ function App() {
   useEffect(() => {
     loadBestsellers();
     getReadingListFromDB();
-  }, []);
+  }, [loadBestsellers]);
 
   return (
     <div className="App">
       <TopNav />
       <div className="mainContent">
         <SideNav />
-        <Bestsellers />
+        <Bestsellers bestsellerList={bestsellerList} />
       </div>
     </div>
   );
--- src/Bestsellers.tsx
+++ src/Bestsellers.tsx
@@ -1,10 +1,26 @@
-export const Bestsellers = () => {
-  return (
-    <div className="d-flex flex-row" id="top">
-      <div id="bestsellerLists" className="topMargin">
-        <p>Main Content</p>
-        <p>Get rid of bootstrap stuff!</p>
+// import { useEffect } from "react";
+import { Bestseller } from "./App";
+
+interface BestsellerProps {
+  bestsellerList: Bestseller[] | null;
+}
+
+export const Bestsellers = ({ bestsellerList }: BestsellerProps) => {
+  // useEffect(() => {
+  //   console.log("props: ", bestsellerList);
+  // }, [bestsellerList]);
+
+  if (bestsellerList) {
+    console.log(bestsellerList[0].title);
+    return (
+      <div className="d-flex flex-row" id="top">
+        <div id="bestsellerLists" className="topMargin">
+          <p>Main Content</p>
+          <p>{bestsellerList[0].title}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 };
